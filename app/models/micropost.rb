828e7380ad05952c001141430d2d6a26e53c054f
ureliot <manuelureta@gmail.com>
2012-07-19 15:32:08 -0400

    first commit

--- app/models/micropost.rb
+++
@@ -1,3 +0,0 @@
-class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 AppDemo1::Application.routes.draw do
-  resources :microposts
-
   resources :users
 
   # The priority is based upon order of creation:
